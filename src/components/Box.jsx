3d7a8e48c6ad9f8e5b6b68fd34e6e69707613f97
Eunjee <58850132+wowww@users.noreply.github.com>
2022-02-23 15:48:00 +0900

    feat. 조명에 따라 박스의 빛이 변하는 meshNormalMaterial

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -11,8 +11,7 @@ function Box() {
   return (
     <mesh rotation={[90, 0, 20]}>
       <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
-      {/* <meshLambertMaterial attach="material" color="blue"/> */}
-      <meshStandardMaterial map={colorMap} />
+      <meshNormalMaterial attach="material" />
     </mesh>
   )
 }
