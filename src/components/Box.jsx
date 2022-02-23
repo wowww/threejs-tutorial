a61517c2612175dda47ea34c50741aef6436e0a8
Eunjee <58850132+wowww@users.noreply.github.com>
2022-02-23 15:42:08 +0900

    feat. 질감 표현을 할 수 있는 meshStandardMaterial

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import texture from '../images/map.jpg'
+import { MeshStandardMaterial } from 'three';
+
 function Box() {
+  const colorMap = useLoader(TextureLoader, texture)
+  
   return (
     <mesh rotation={[90, 0, 20]}>
       <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
-      <meshLambertMaterial attach="material" color="blue"/>
+      {/* <meshLambertMaterial attach="material" color="blue"/> */}
+      <meshStandardMaterial map={colorMap} />
     </mesh>
   )
 }
