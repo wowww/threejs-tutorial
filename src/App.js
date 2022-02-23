fcf83838c7f0ff456e2feb499ca1c89df938de9e
Eunjee <58850132+wowww@users.noreply.github.com>
2022-02-23 15:26:00 +0900

    docs. 캔버스에 Box, OrbitControls(마우스로 움직임), ambientLight와 directionalLight(빛 관련) 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import styled from "styled-components";
 
 import Background from "./components/Background";
 import TextSection from "./components/TextSection";
+import Box from './components/Box';
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei"
 
 export default function App() {
   return (
     <Wrapper className="App">
       <Background />
       <TextSection />
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Box />
+      </Canvas>
     </Wrapper>
   );
 }
@@ -16,4 +26,8 @@ export default function App() {
 const Wrapper = styled.div`
   position: relative;
   background: #1f1144;
+
+  canvas {
+    height: 500px;
+  }
 `;
