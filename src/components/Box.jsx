6be256cc9c9cf7a705297c3c3340a2c18f2ff247
Eunjee <58850132+wowww@users.noreply.github.com>
2022-02-23 15:25:11 +0900

    docs. 정사각형 박스 추가

---
+++ src/components/Box.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Box() {
+  return (
+    <mesh rotation={[90, 0, 20]}>
+      <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
+      <meshLambertMaterial attach="material" color="blue"/>
+    </mesh>
+  )
+}
+
+export default Box
