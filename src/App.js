8306dff29238be7d6053066abbca328e6df67d6e
Eunjee <58850132+wowww@users.noreply.github.com>
2022-02-24 11:28:15 +0900

    chore. Pipeline을 이용하여 gltf 파일 js 변환하고 화면에 랜더링
    - npm install -g gltf-pipeline
    - gltf-pipeline -i iphone.gltf -o iphone.gltf -d
    -npx gltfjsx iphone.gltf

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Box from './components/Box';
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Sphere from './components/AnimateSphere';
+import Iphone from './components/Iphone';
 
 export default function App() {
   return (
@@ -29,6 +30,14 @@ export default function App() {
         <directionalLight position={[-2, 5, 2]} intensity={1} />
         <Suspense fallback={null}>
           <Sphere />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Iphone />
         </Suspense>
       </Canvas>
     </Wrapper>
